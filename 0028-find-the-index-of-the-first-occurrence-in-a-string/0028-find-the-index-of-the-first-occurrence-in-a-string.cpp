bd078d9998a2fd8776a8aad937403d9c10809a42
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2023-08-16 20:43:36 +0530

    Time: 3 ms (41.17%), Space: 6.3 MB (23.8%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int strStr(string h, string n) {
+        if(n.size()>h.size()){
+               return -1;
+           }
+           int j=0;
+       for(int i=0;i<h.size()-n.size()+1;i++){
+           
+           if(h[i]==n[0]){
+            int l=i;
+            while(j<n.size() && h[l]==n[j]){
+                l++;
+                j++;
+            }
+            if(j==n.size()){
+                return i;
+            }
+           }
+
+           j=0;
+
+           
+       }
+       return -1;
+
+    }
+};
