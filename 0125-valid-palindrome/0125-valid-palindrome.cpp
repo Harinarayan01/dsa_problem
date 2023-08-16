83c284e61a0ba340bd00fe34af9e810405964eab
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2023-08-16 21:36:16 +0530

    Time: 6 ms (48.42%), Space: 7.8 MB (21.34%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string t="";
+        
+        
+        for(char x:s){
+            
+            if(isalpha(x) || isdigit(x)){
+
+            t+=tolower(x);
+            }
+        }
+        
+        string g=t;
+        reverse(g.begin(),g.end());
+        
+        return t==g;
+        
+        
+    }
+};
+   
