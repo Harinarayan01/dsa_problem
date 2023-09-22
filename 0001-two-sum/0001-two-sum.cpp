1d47b10738009025a6fa2012681853c62f09aecf
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2023-09-22 07:47:31 +0530

    Time: 414 ms (12.76%), Space: 10.4 MB (54.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,12 +4,12 @@ public:
         vector<int>ans;
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
-                if((nums[i]+nums[j])==target){
-                   ans.push_back(i);
-                   ans.push_back(j);
+                if(nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
                 }
             }
         }
-    return ans;    
+        return ans;    
     }
 };
