ec417f8ddd232305f4157e18ee35fd969a66c32c
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2025-01-10 18:59:11 +0530

    Time: 5 ms (89.21%), Space: 57.7 MB (89.19%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,5 +3,13 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    return x===+x.toString().split("").reverse().join("");
+    let reverse=0;
+    let original=x;
+    
+    while(x>0){
+        let dil=x%10;
+        reverse=reverse*10+dil;
+        x=Math.floor(x/10);
+    }
+    return original===reverse;
 };
